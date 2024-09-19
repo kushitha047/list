bf2461e2fef2a022b58acfb0fd3417e5f5361920
kushitha047 <22hr1a3234@mtieat.org>
2024-09-19 16:16:22 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,11 +5,6 @@ Sample Input:
 Sample Output:
 10 20 30 40 50 
 ***
-# Read the list elements from the input line
 elements = list(map(int, input().split()))
-
-# Sort the list
 sorted_elements = sorted(elements)
-
-# Print the sorted list elements space-separated followed by "program"
 print(" ".join(map(str, sorted_elements)), "program")
