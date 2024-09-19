61876d4ddeec6b21997dcfb979f8f2b2ceaca44d
kushitha047 <22hr1a3234@mtieat.org>
2024-09-19 16:06:53 +0530

    Update even&odd.py

--- even&odd.py
+++ even&odd.py
@@ -19,21 +19,12 @@ Sample Output:
 The even list [2, 6]
 The odd list [1, 3, 5]
 ***
-# Read the size of the list
 size = int(input())
-
-# Initialize an empty list to store the elements
 elements = []
-
-# Read the next `size` number of elements and append them to the list
 for _ in range(size):
     element = int(input())
     elements.append(element)
-
-# Separate the elements into even and odd lists
 even_list = [x for x in elements if x % 2 == 0]
 odd_list = [x for x in elements if x % 2 != 0]
-
-# Print the results in the required format
 print("The even list", even_list)
 print("The odd list", odd_list)
